edca2d077625eb87e2c57445538bcbc5c727a0ec
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-02-22 15:05:43 +0200

    added template for formsku component

--- src/app/forms/form-scu.component.ts
+++ src/app/forms/form-scu.component.ts
@@ -1,12 +1,28 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
 
-@Component ({
+@Component({
   selector: "form-scu",
   template: `
      <h2>Demo form will be here..</h2>
      <div class="ui raised segment">
-        <h2 class="ui header">Demo Form SCU</h2>
-</div>
+        <h2 class="ui header">Demo Form SKU</h2>
+        <form class="ui form"
+              #myForm1="ngForm"
+              (ngSubmit)="onSubmit(myForm1.value)">
+        
+          <div class="field">
+            <label for="inputSKU">SKU</label>
+            <input type="text"
+                   id="inputSKU"
+                   placeholder="SKU"
+                   name="SKU"
+                   ngModel> 
+          </div>
+          
+          <button class="ui button positive" type="submit">Send</button>
+        </form>
+     </div>
   `
 })
-export class FormSCU{}
+export class FormSCU {
+}
