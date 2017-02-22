622d1c08735ce79688782593ccdb32997889e505
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-02-22 14:22:57 +0200

    added some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Hello {{name}}</h1>
-    <forms-list></forms-list>
+    <div class="ui main text container">
+      <h1>Hello {{name}}</h1>
+      <forms-list></forms-list>
+    </div>
   `,
 })
 export class AppComponent  { name = 'Angular'; }
--- src/app/forms/form-scu.component.ts
+++ src/app/forms/form-scu.component.ts
@@ -4,6 +4,9 @@ import { Component } from "@angular/core";
   selector: "form-scu",
   template: `
      <h2>Demo form will be here..</h2>
+     <div class="ui raised segment">
+        <h2 class="ui header">Demo Form SCU</h2>
+</div>
   `
 })
 export class FormSCU{}
